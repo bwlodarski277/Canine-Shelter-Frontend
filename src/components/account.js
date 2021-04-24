a1bf2a02fb1a11fbe639ed4cd3c6e9088d67c3d9
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-04-25 00:25:48 +0100

    Added account deletion

--- src/components/account.js
+++ src/components/account.js
@@ -1,10 +1,12 @@
-import { Avatar, Button, Card, Divider, Form, Input, message, Upload } from 'antd';
+import { Avatar, Button, Card, Divider, Form, Input, message, Modal, Space, Upload } from 'antd';
 import Title from 'antd/lib/typography/Title';
 import React, { Component } from 'react';
-import { UploadOutlined } from '@ant-design/icons';
+import { ExclamationCircleFilled, UploadOutlined } from '@ant-design/icons';
+import PropTypes from 'prop-types';
 import UserContext from '../contexts/user';
 import { status } from '../helpers/fetch';
 const { Item } = Form;
+const { confirm } = Modal;
 
 const usernameRules = [
 	{
@@ -58,14 +60,10 @@ const confirmRules = [
 export class Account extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
-	}
-
-	componentDidMount() {
-		const { user, jwt } = this.context;
-		this.setState({
-			user,
-			jwt,
+		const { jwt } = this.props.context;
+		this.state = {
+			popupVisible: false,
+			form: React.createRef(),
 			upload: {
 				accept: 'image/jpeg, image/png',
 				maxCount: 1,
@@ -75,12 +73,21 @@ export class Account extends Component {
 				headers: { Authorization: 'Bearer ' + jwt },
 				onChange: this.onPictureChanged
 			}
-		});
+		};
+		this.confirmDelete = this.confirmDelete.bind(this);
+		this.onPictureChanged = this.onPictureChanged.bind(this);
+		this.onPictureRemoved = this.onPictureRemoved.bind(this);
+		this.onInfoChanged = this.onInfoChanged.bind(this);
+	}
+
+	onPictureRemoved() {
+		const { setUser } = this.props.context;
+		setUser({ imageUrl: null });
 	}
 
 	onPictureChanged(info) {
 		if (info.file.status === 'done') {
-			const { user, jwt } = this.state;
+			const { user, jwt, setUser } = this.props.context;
 			console.log(info);
 			const res = info.file.response;
 			fetch(user.links.self, {
@@ -92,7 +99,7 @@ export class Account extends Component {
 				body: JSON.stringify({ imageUrl: res.link })
 			})
 				.then(status)
-				.then(() => this.context.setUser({ imageUrl: res.link }))
+				.then(() => setUser({ imageUrl: res.link }))
 				.catch(error => {
 					message.error(`${info.file.name} file upload failed.`);
 					console.error(error);
@@ -103,63 +110,118 @@ export class Account extends Component {
 		}
 	}
 
+	/**
+	 * Calls the user update function in the root component
+	 * @param {object} data values passed by the form that called this
+	 */
 	onInfoChanged(data) {
-		this.context.setUser(data);
+		const { form } = this.state;
+		this.props.context.setUser(data, form);
+	}
+
+	/**
+	 * Spawns a confirmation dialog when user wants to delete account
+	 */
+	confirmDelete() {
+		const { user, jwt, logout } = this.props.context;
+		confirm({
+			title: 'Are you sure?',
+			icon: <ExclamationCircleFilled />,
+			content: 'This will delete your account forever!',
+			okType: 'danger',
+			okText: 'Yes',
+			cancelText: 'No',
+			onOk() {
+				fetch(user.links.self, {
+					method: 'DELETE',
+					headers: { Authorization: 'Bearer ' + jwt }
+				})
+					.then(status)
+					.then(() => logout(true)) // Indicating account is deleted
+					.catch(error => {
+						console.error(error);
+						message.error('Could not delete account');
+					});
+			}
+		});
 	}
 
 	render() {
-		const { user } = this.context;
+		const { user, loggedIn } = this.props.context;
+		if (!loggedIn) return <Title style={{ textAlign: 'center' }}>Loading...</Title>;
 		return (
 			<section style={{ maxWidth: '700px', margin: 'auto' }}>
-				{this.context.loggedIn && (
-					<Card style={{ textAlign: 'center' }}>
-						<Title>Your Account</Title>
-						<Avatar size={128} src={user.imageUrl} />
-						<Divider />
-						<Upload {...this.state.upload} onChange={this.onPictureChanged.bind(this)}>
-							<Title level={3}>Avatar</Title>
-							<Button icon={<UploadOutlined />}>Upload avatar</Button>
+				<Card style={{ textAlign: 'center' }}>
+					<Title>Your Account</Title>
+					<Avatar size={128} src={user.imageUrl} />
+					<Divider />
+					<Title level={3}>Avatar</Title>
+					<Space>
+						<Upload {...this.state.upload} onChange={this.onPictureChanged}>
+							<Button type="primary" icon={<UploadOutlined />}>
+								Upload avatar
+							</Button>
 						</Upload>
-						<Divider />
-						<Title level={3}>Update Profile</Title>
-						<Form
-							labelCol={{ span: 8 }}
-							wrapperCol={{ span: 16 }}
-							onFinish={this.onInfoChanged.bind(this)}
-						>
-							<Item label="Username" name="username" rules={usernameRules}>
-								<Input placeholder={user.username} />
-							</Item>
-							<Item label="Email" name="email" rules={emailRules}>
-								<Input placeholder={user.email} />
-							</Item>
-							<Item label="First name" name="firstName" rules={firstRules}>
-								<Input placeholder={user.firstName} />
-							</Item>
-							<Item label="Last name" name="lastName" rules={lastRules}>
-								<Input placeholder={user.lastName} />
-							</Item>
-							<Item label="Password" name="password" rules={passwordRules}>
-								<Input.Password placeholder="Password" />
-							</Item>
-							<Item label="Confirm" name="confirm" rules={confirmRules}>
-								<Input.Password placeholder="Confirm password" />
-							</Item>
-							<Item wrapperCol={{ offset: 4, span: 16 }}>
-								<Button type="primary" htmlType="submit">
-									Update profile
-								</Button>
-							</Item>
-						</Form>
-						<Divider />
-						<Button onClick={this.context.logout}>Log out</Button>
-					</Card>
-				)}
+						<Button type="text" onClick={this.onPictureRemoved}>
+							Delete avatar
+						</Button>
+					</Space>
+					<Divider />
+					<Title level={3}>Update Profile</Title>
+					<Form
+						ref={this.state.form}
+						labelCol={{ span: 8 }}
+						wrapperCol={{ span: 16 }}
+						onFinish={this.onInfoChanged}
+					>
+						<Item label="Username" name="username" rules={usernameRules}>
+							<Input placeholder={user.username} />
+						</Item>
+						<Item label="Email" name="email" rules={emailRules}>
+							<Input placeholder={user.email} />
+						</Item>
+						<Item label="First name" name="firstName" rules={firstRules}>
+							<Input placeholder={user.firstName} />
+						</Item>
+						<Item label="Last name" name="lastName" rules={lastRules}>
+							<Input placeholder={user.lastName} />
+						</Item>
+						<Item label="Password" name="password" rules={passwordRules}>
+							<Input.Password placeholder="Password" />
+						</Item>
+						<Item label="Confirm" name="confirm" rules={confirmRules}>
+							<Input.Password placeholder="Confirm password" />
+						</Item>
+						<Item wrapperCol={{ offset: 4, span: 16 }}>
+							<Button type="primary" htmlType="submit">
+								Update profile
+							</Button>
+						</Item>
+					</Form>
+					<Divider />
+					<Space>
+						<Button onClick={this.props.context.logout}>Log out</Button>
+						<Button danger onClick={this.confirmDelete}>
+							Delete account
+						</Button>
+					</Space>
+				</Card>
 			</section>
 		);
 	}
 }
 
+Account.propTypes = {
+	context: PropTypes.object
+};
+
 Account.contextType = UserContext;
 
-export default Account;
+/**
+ * Wrapper for the Account element, passes context to the item.
+ */
+const AccountWrapper = () => {
+	return <UserContext.Consumer>{context => <Account context={context} />}</UserContext.Consumer>;
+};
+
+export default AccountWrapper;
