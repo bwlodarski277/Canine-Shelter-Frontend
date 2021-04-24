e0f77dc4ed315a5d21f237509aeb4e2724640468
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-04-25 00:26:58 +0100

    Credit icon

--- src/components/iconCredit.js
+++ src/components/iconCredit.js
@@ -1,17 +1,31 @@
+import { TwitterOutlined } from '@ant-design/icons';
 import React from 'react';
+
+const twitter = 'https://twitter.com/CanineShelter';
 
 const IconCredit = () => {
 	return (
-		<div>
-			Icons made by{' '}
-			<a href="https://www.freepik.com" title="Freepik">
-				Freepik
-			</a>{' '}
-			from{' '}
-			<a href="https://www.flaticon.com/" title="Flaticon">
-				www.flaticon.com
-			</a>
-		</div>
+		<>
+			<section style={{ float: 'left' }}>
+				Icons made by{' '}
+				<a href="https://www.freepik.com" title="Freepik" target="_blank" rel="noreferrer">
+					Freepik
+				</a>{' '}
+				from{' '}
+				<a
+					href="https://www.flaticon.com/"
+					title="Flaticon"
+					target="_blank"
+					rel="noreferrer"
+				>
+					www.flaticon.com
+				</a>
+			</section>
+			<TwitterOutlined
+				style={{ float: 'right' }}
+				onClick={() => window.open(twitter, '_blank')}
+			/>
+		</>
 	);
 };
 
