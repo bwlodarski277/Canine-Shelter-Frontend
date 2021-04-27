bb7d3c04105284d38f2e8f6747b29e87ed629b98
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-04-27 01:52:23 +0100

    Created breeds componenet

--- src/components/breeds.js
+++ src/components/breeds.js
@@ -1,6 +1,7 @@
+/* eslint-disable func-style */
 import React from 'react';
-import { json, status } from '../helpers/fetch';
-import { Card } from 'antd';
+import { error, json, status } from '../helpers/fetch';
+import { Card, message } from 'antd';
 import SearchList from './searchList';
 import { withRouter } from 'react-router-dom';
 import BreedListItem from './breedListItem';
@@ -30,14 +31,46 @@ const getBreeds = async (currentPage, pageSize, query, order, direction) => {
 	return { list: breeds, count };
 };
 
+// Requires function keyword for `this`
+// eslint-disable-next-line func-style
+const updateBreed = (data, breedUrl, context, form) => {
+	if (data.name || data.description)
+		fetch(breedUrl, {
+			method: 'PUT',
+			headers: { Authorization: 'Bearer ' + context.jwt, 'Content-Type': 'application/json' },
+			body: JSON.stringify(data)
+		})
+			.then(status)
+			.then(json)
+			.then(() => {
+				message.success('Breed updated');
+				form.current.resetFields();
+				this.getData();
+			})
+			.catch(error);
+	else message.info('Please enter at least one field to update.');
+};
+
 /**
  * Maps the list of breeds to a set of React componenets.
  * @param {List<object>} breeds list of breed objects
  */
-const mapBreeds = (breeds, { goToBreed }) => {
-	const breedList = breeds.map(breed => (
-		<BreedListItem key={breed.id} breed={breed} onClick={() => goToBreed(breed.id)} />
-	));
+const mapBreeds = (breeds, { goToBreed, updateBreed }, _, context) => {
+	const { user = {} } = context;
+	const breedList = breeds.map(breed => {
+		return (
+			<>
+				<BreedListItem
+					key={breed.id}
+					breed={breed}
+					user={user}
+					onClick={() => goToBreed(breed.id)}
+					updateBreed={updateBreed}
+					context={context}
+				/>
+			</>
+		);
+	});
 	return (
 		<section style={{ maxWidth: '900px', margin: 'auto', marginTop: '1em', minHeight: '70vh' }}>
 			<Card style={{ marginBottom: '1em' }}>{breedList}</Card>
@@ -65,7 +98,7 @@ const Breeds = props => {
 			columns={columns}
 			fetchFunction={getBreeds}
 			listMapping={mapBreeds}
-			extraFuncs={{ goToBreed }}
+			extraFuncs={{ goToBreed, updateBreed }}
 			{...props}
 		/>
 	);
